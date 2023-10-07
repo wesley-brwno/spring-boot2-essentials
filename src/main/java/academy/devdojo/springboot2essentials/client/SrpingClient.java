e1143fcc270d2e6fc777020525903c10d0c3b9f6
wesley <wesleybruno910@gmail.com>
2023-10-07 10:41:36 -0300

    Put, Delete com RestTemplate

--- src/main/java/academy/devdojo/springboot2essentials/client/SrpingClient.java
+++ src/main/java/academy/devdojo/springboot2essentials/client/SrpingClient.java
@@ -3,8 +3,7 @@ package academy.devdojo.springboot2essentials.client;
 import academy.devdojo.springboot2essentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -29,5 +28,36 @@ public class SrpingClient {
                 new ParameterizedTypeReference<List<Anime>>() {});
 
         log.info(exchange.getBody());
+
+//        Anime kingdom = Anime.builder().name("Kingdom").build();
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
+//        log.info("Anime saved {}", kingdomSaved);
+
+        Anime chunibyou = Anime.builder().name("Chunibyou").build();
+        ResponseEntity<Anime> chunibyouSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.POST,
+                new HttpEntity<>(chunibyou, createJsonHeader()),
+                Anime.class);//.getBody
+        log.info("Anime saved {}", chunibyouSaved);
+
+        Anime animeToBeUpdated = chunibyouSaved.getBody();
+        animeToBeUpdated.setName("chuunibyou demo koi ga shitai");
+
+        ResponseEntity<Void> chunnibyouUpdated = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.PUT,
+                new HttpEntity<>(animeToBeUpdated, createJsonHeader()),
+                Void.class);
+        log.info(chunnibyouUpdated);
+
+        System.err.println("-----------------Deleting: "+ animeToBeUpdated.getId());
+        new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class, animeToBeUpdated.getId());
+    }
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
